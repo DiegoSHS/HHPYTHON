cd478d4b651e61cd0f1fc4fe86ce28f5a4b12017
Diego López Ariza <diegoawesome2017@outlook.com>
2024-06-17 22:32:35 -0600

    Updated to folow pep 8

--- app/routes.py
+++ app/routes.py
@@ -1,158 +1,154 @@
-from flask import request, Blueprint
-from flask_cors import cross_origin
-import json
-from brutesleuth import BruteChain
-import string
-import threading
-import zipfile
-import re
+"""This module contains the routes for the Flask app"""
+
 import time
+import json
+import string
+import re
+import threading
 from queue import Queue
 from random import shuffle
-
+from zipfile import ZipFile, error
+from brutesleuth import BruteChain
+from flask import request, Blueprint
+from flask_cors import cross_origin
 main = Blueprint('main', __name__)
-passwords = Queue()
-finalpass = None
-threads = []
-tests = 0
-filename = ''
-zf = None
 
-
-def timeit(f):
+def timeit(function):
     """A decorator to print the time a function took to run"""
     def wrapper(*args, **kwargs):
         """The wrapper that is run instead of the function"""
-        t0 = time.time()
-        ret = f(*args, **kwargs)
-        t1 = time.time()
-        print('Time spent: {} seconds'.format(t1 - t0))
-        return ret
+        start_time = time.time()
+        updated_function = function(*args, **kwargs)
+        end_time = time.time()
+        print(f'Time spent: {end_time - start_time} seconds')
+        return updated_function
     return wrapper
 
 
-def calculate(pswdict, filename):
-    zf = zipfile.ZipFile(filename)
-    global finalpass, tests
-    for psw in pswdict:
-        if finalpass != None:
-            break
-        try:
-            zf.setpassword(psw.encode())
-            if zf.testzip() is None:
-                finalpass = psw
-                break
-        except:
-            pass
-
-
-def createThreads(limit):
-    global threads
+def setup_threads(function,limit:int):
+    """Setup threads for the function passed in the arguments"""
+    threads = []
     for _ in range(1, limit):
-        thread = threading.Thread(target=worker, args=(_,), daemon=True)
+        thread = threading.Thread(target=function, args=(_,), daemon=True)
         thread.start()
         threads.append(thread)
+    return threads
 
 
-def joinThreads():
-    global threads
+def join_threads(threads:list):
+    """Join the threads passed in the arguments"""
     for thread in threads:
         thread.join()
 
-
-def split(a, n):
-    k, m = divmod(len(a), n)
-    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
-
-
-def worker(w):
-    global finalpass, zf, filename
-    while not passwords.empty():
-        password = passwords.get()
-        try:
-            zf.extractall(
-                path='/tmp/',
-                pwd=password.encode())
-            finalpass = password
-            with passwords.mutex:
-                passwords.all_tasks_done()
-                passwords.queue.clear()
-            return
-        except:
-            pass
-        pass
-
-
-def startThreads():
-    length = passwords.qsize()
+def start_threads(passwords_queue:Queue,function):
+    """Start the threads for the function passed in the arguments"""
+    length = passwords_queue.qsize()
     print(f'Dict size {length}')
-    createThreads(4)
+    return setup_threads(limit=4,function=function)
 
 
-def iftr(string):
-    if string == 'true':
-        return True
-    else:
-        return False
+def split(pass_list:list, num_parts:int):
+    """Split a list into n parts"""
+    kdiv, module = divmod(len(pass_list), num_parts)
+    return (pass_list[i*kdiv+min(i, module):(i+1)*kdiv+min(i+1, module)] for i in range(num_parts))
+
+
+def iftr(text:string):
+    """Convert a string to a boolean value"""
+    return text == 'true'
 
 
 @timeit
-def reorder(iterable):
-    passws = [p for p in list(iterable) if not re.findall(
+def reorder(iterable, passwords_queue:Queue):
+    """Reorder the dictionary to filter out the passwords that are not valid"""
+    pass_words = [p for p in list(iterable) if not re.findall(
         pattern=r'(.)\1{2}|(.)\1{3}|(.)\1{4}|(.)\1{5}|(.)\1{6}|(.)\1{7}|(.)\1{8}', string=p
     )]
-    shuffle(passws)
-    passwords.queue.extend(passws)
+    shuffle(pass_words)
+    passwords_queue.queue.extend(pass_words)
     print('Filtered')
 
 
-def generate(len=3, dig=True, lw=False, up=False,  sp=False, ct=''):
-    if ct != '':
-        return BruteChain(len, list(bytes.fromhex(ct).decode('utf-8')))
-    if up and lw and dig and sp:
-        return BruteChain(len, list(string.printable))
-    if up and lw and dig:
-        return BruteChain(len, list(string.ascii_letters+string.digits))
-    if up and lw:
-        return BruteChain(len, list(string.ascii_letters))
-    if up and dig:
-        return BruteChain(len, list(string.ascii_uppercase+string.digits))
-    if lw and dig:
-        return BruteChain(len, list(string.ascii_lowercase+string.digits))
-    if up:
-        return BruteChain(len, list(string.ascii_uppercase))
-    if lw:
-        return BruteChain(len, list(string.ascii_lowercase))
-    if dig:
-        return BruteChain(len, list(range(0, 10)))
-    else:
-        return BruteChain(len, list(string.printable))
+def generate_dictionary(
+    length=3,
+    digits=True,
+    lowercase=False,
+    uppercase=False,
+    marks=False,
+    custom_string=''
+    ):
+    """Generate a dictionary for the bruteforce attack on a zip file"""
+    if custom_string != '':
+        return BruteChain(length, list(bytes.fromhex(custom_string).decode('utf-8')))
+    if uppercase and lowercase and digits and marks:
+        return BruteChain(length, list(string.printable))
+    if uppercase and lowercase and digits:
+        return BruteChain(length, list(string.ascii_letters+string.digits))
+    if uppercase and lowercase:
+        return BruteChain(length, list(string.ascii_letters))
+    if uppercase and digits:
+        return BruteChain(length, list(string.ascii_uppercase+string.digits))
+    if lowercase and digits:
+        return BruteChain(length, list(string.ascii_lowercase+string.digits))
+    if uppercase:
+        return BruteChain(length, list(string.ascii_uppercase))
+    if lowercase:
+        return BruteChain(length, list(string.ascii_lowercase))
+    if digits:
+        return BruteChain(length, list(range(0, 10)))
+    return BruteChain(length, list(string.printable))
 
 
 @main.route('/bruteforce/', methods=['POST'])
 @cross_origin()
 def bruteforce():
-    global threads, filename, zf, finalpass
-    finalpass = None
-    threads = []
+    """The route for the bruteforce attack on a zip file"""
+    passwords = Queue()
+    correct_password = {"password":""}
+    def worker(correct_password:dict, zip_file: ZipFile, passwords_queue:Queue):
+        while not passwords_queue.empty():
+            password = passwords_queue.get()
+            try:
+                zip_file.extractall(
+                    path='/tmp/',
+                    pwd=password.encode())
+                correct_password.update(password=password)
+                with passwords_queue.mutex:
+                    passwords_queue.all_tasks_done()
+                    passwords_queue.queue.clear()
+                return
+            except error:
+                pass
+
     print('starts')
     file = request.files['file']
-    typee = request.form['type']
-    argum = json.loads(typee)
-    reorder(iterable=generate(
-        len=int(argum['len']),
-        dig=iftr(argum['dig']),
-        lw=iftr(argum['lw']),
-        up=iftr(argum['up']),
-        sp=iftr(argum['sp']),
-        ct=argum['ct']
-    ))
+    password_type = request.form['type']
+    arguments = json.loads(password_type)
+    new_passwords = generate_dictionary(
+        length=int(arguments['len']),
+        digits=iftr(arguments['dig']),
+        lowercase=iftr(arguments['lw']),
+        uppercase=iftr(arguments['up']),
+        marks=iftr(arguments['sp']),
+        custom_string=arguments['ct']
+        )
+    reorder(
+        iterable=new_passwords,
+        passwords_queue=passwords
+        )
     filename = './app/uploads/' + file.filename
     file.save(filename)
-    zf = zipfile.ZipFile(filename, 'r')
-    startThreads()
-    joinThreads()
-    print(f'Enviando clave al servidor: {finalpass}')
+    with ZipFile(filename, 'r') as zip_file:
+        threads = start_threads(
+            passwords_queue=passwords,
+            function=lambda _: worker(
+                zip_file=zip_file,
+                correct_password=correct_password,
+                passwords_queue=passwords
+                )
+            )
+        join_threads(threads)
+    print(f'Enviando clave al servidor: {correct_password.get("password")}')
     return {
-        "password": finalpass
+        "password": correct_password.get('password')
     }
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+"""This file is the entry point of the application"""
 from app import setup
 
 app = setup()
